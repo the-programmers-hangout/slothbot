10a921abe353b461ab37638d4ae75ba0520146c8
ianagbip1oti <ianagbip1oti@gmail.com>
2020-12-15 07:18:26 +0000

    📦 NEW:  Allow explicit channel id option for ratelimit command

--- src/main/java/com/github/princesslana/slothbot/commands/RateLimitCommand.java
+++ src/main/java/com/github/princesslana/slothbot/commands/RateLimitCommand.java
@@ -7,6 +7,10 @@ import disparse.discord.AbstractPermission;
 import disparse.discord.smalld.DiscordRequest;
 import disparse.discord.smalld.DiscordResponse;
 import disparse.parser.reflection.CommandHandler;
+import disparse.parser.reflection.Flag;
+import disparse.parser.reflection.ParsedEntity;
+import disparse.parser.reflection.Usage;
+import disparse.parser.reflection.Usages;
 
 public class RateLimitCommand {
 
@@ -18,17 +22,44 @@ public class RateLimitCommand {
     this.limiter = limiter;
   }
 
+  @ParsedEntity
+  private static class Options {
+    @Flag(
+        shortName = 'c',
+        longName = "channel",
+        description =
+            "The id of the channel to rate limit. "
+                + "Defaults to the current channel if not provided.")
+    public String channelId;
+  }
+
   @CommandHandler(
       commandName = "ratelimit",
-      perms = {AbstractPermission.ADMINISTRATOR})
-  public DiscordResponse count() {
+      perms = {AbstractPermission.ADMINISTRATOR},
+      description = "Apply a rate limit to the current or specified channel.")
+  @Usages({
+    @Usage(
+        usage = "120",
+        description = "Set a rate limit of 120 messages per minute on the current channel"),
+    @Usage(
+        usage = "-c <channel_id> 60",
+        description =
+            "Set a rate limit of 60 messages per minute on the channel identified by channel_id"),
+    @Usage(
+        usage = "-c <channel_id> 0",
+        description = "Clear the rate limit on the channel identified by channel_id")
+  })
+  public DiscordResponse count(Options opts) {
     return Try.run(
         () -> {
           Preconditions.checkArgument(
               request.getArgs().size() == 1, "You must include a rate limit");
 
           var rateLimit = Integer.parseInt(request.getArgs().get(0));
-          var channelId = request.getDispatcher().channelFromEvent(request.getEvent());
+          var channelId =
+              opts.channelId == null
+                  ? request.getDispatcher().channelFromEvent(request.getEvent())
+                  : opts.channelId;
 
           Preconditions.checkArgument(rateLimit >= 0, "Rate limit must be >= 0");
 
